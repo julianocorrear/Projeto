d1cb9b9df780af733c8bae8aeb17579e9398d80d
julianocorrear <jcorrear@edu.unisinos.br>
2021-08-30 20:36:29 -0300

    Abre PDF implementado

--- Metodos.cs
+++ Metodos.cs
@@ -1,12 +1,38 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace Classes
 {
     public class Metodos
     {
             #region CarregaPDF
+
+            public void CarregaPDF(string vardocname)
+            {
+                using (PdfReader reader = new PdfReader (@"C:\Temp\"+vardocname))
+                {
+                    //var textoDoPdf = new System.Text.StringBuilder();
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        string aux = PdfTextExtractor.GetTextFromPage(reader, i);
+                        string[] linhasDoPdf = aux.Split('\n');
+                        foreach(string linha in linhasDoPdf)
+                        {
+
+                        }
+                        
+                    }
+                    
+                }
+
+            }
     
+            #endregion
+            #region Pesquisa1Palavra
+            
             #endregion
     }
 
