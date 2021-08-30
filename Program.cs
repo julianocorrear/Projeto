6b5a03e8fb030fe13799fcd91e8893c50965bf17
julianocorrear <jcorrear@edu.unisinos.br>
2021-08-29 21:14:39 -0300

    Teste1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
-namespace Projeto
+//using Metodos;
+namespace Classes
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //var Interface1 = new interface();
             Console.WriteLine("Hello World!");
         }
     }
